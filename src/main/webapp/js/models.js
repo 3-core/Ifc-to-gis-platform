372d8a55b5decc9fdc429aa6472b59f229e55220
BreezeBM <seyya77@gmail.com>
2023-09-05 16:52:54 +0900

    refactor: 시흥정류장 model matrix 제거

--- src/main/webapp/js/models.js
+++ src/main/webapp/js/models.js
@@ -1,20 +1,3 @@
-const subway_siheung_matrix = new Ditap.Transforms.headingPitchRollToFixedFrame(
-    new Ditap.Cartesian3.fromDegrees(
-        126.90155165017461,
-        37.45330471900097,
-        55.9029385
-    ),
-    new Ditap.HeadingPitchRoll(Cesium.Math.toRadians(0), 0, 0),
-    Ditap.Ellipsoid.WGS84,
-    Ditap.Transforms.localFrameToFixedFrameGenerator("south", "east")
-);
-
-const subway_siheung_model = Ditap.Model.fromGltf({
-    id: "subway_siheung",
-    url: "public/glb/subway_siheung/subway_siheung.glb",
-    modelMatrix: subway_siheung_matrix,
-});
-
 const jeonju_model_matrix = new Ditap.Transforms.headingPitchRollToFixedFrame(
     new Ditap.Cartesian3.fromDegrees(
         127.065465892762,
