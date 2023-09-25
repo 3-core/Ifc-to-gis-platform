07b6bcd7b084bfc16b6429c2adb412e7e4f5ca6a
BreezeBM <seyya77@gmail.com>
2023-09-25 09:55:28 +0900

    fix: 정류장 데이터로 수정

--- src/main/webapp/js/models.js
+++ src/main/webapp/js/models.js
@@ -1,16 +1,16 @@
-const jeonju_model_matrix = new Ditap.Transforms.headingPitchRollToFixedFrame(
+const siheung_model_matrix = new Ditap.Transforms.headingPitchRollToFixedFrame(
     new Ditap.Cartesian3.fromDegrees(
-        127.065465892762,
-        35.8380850335775,
+        127.058968,
+        35.835069,
         34.97499999999889
     ),
-    new Ditap.HeadingPitchRoll(Cesium.Math.toRadians(0), 0, 0),
+    new Ditap.HeadingPitchRoll(Ditap.Math.toRadians(18), 0, 0),
     Ditap.Ellipsoid.WGS84,
     Ditap.Transforms.localFrameToFixedFrameGenerator("south", "east")
 );
 
-const jeonju_model = Ditap.Model.fromGltf({
+const siheung_model = Ditap.Model.fromGltf({
     id: "lx_jeonju",
-    url: "public/glb/lx_jeonju/lx_jeonju.glb",
-    modelMatrix: jeonju_model_matrix,
+    url: "public/glb/siheung/siheung_demonstration.glb",
+    modelMatrix: siheung_model_matrix,
 });
