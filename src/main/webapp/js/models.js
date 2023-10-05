64d2528838d81bf8751ac03061b92b35fe14c64a
BreezeBM <seyya77@gmail.com>
2023-10-05 11:15:13 +0900

    feat: 층 분리 모델 추가 및 기능 추가

--- src/main/webapp/js/models.js
+++ src/main/webapp/js/models.js
@@ -1,7 +1,24 @@
-const siheung_model_matrix = new Ditap.Transforms.headingPitchRollToFixedFrame(
+//const siheung_model_matrix = new Ditap.Transforms.headingPitchRollToFixedFrame(
+//    new Ditap.Cartesian3.fromDegrees(
+//        127.058968,
+//        35.835169,
+//        34.97499999999889
+//    ),
+//    new Ditap.HeadingPitchRoll(Ditap.Math.toRadians(18), 0, 0),
+//    Ditap.Ellipsoid.WGS84,
+//    Ditap.Transforms.localFrameToFixedFrameGenerator("south", "east")
+//);
+//
+//const siheung_model = Ditap.Model.fromGltf({
+//    id: "siheung_model",
+//    url: "public/glb/siheung/siheung_demonstration.glb",
+//    modelMatrix: siheung_model_matrix,
+//});
+
+const siheung_floor_model_matrix = new Ditap.Transforms.headingPitchRollToFixedFrame(
     new Ditap.Cartesian3.fromDegrees(
         127.058968,
-        35.835069,
+        35.835169,
         34.97499999999889
     ),
     new Ditap.HeadingPitchRoll(Ditap.Math.toRadians(18), 0, 0),
@@ -9,8 +26,28 @@ const siheung_model_matrix = new Ditap.Transforms.headingPitchRollToFixedFrame(
     Ditap.Transforms.localFrameToFixedFrameGenerator("south", "east")
 );
 
-const siheung_model = Ditap.Model.fromGltf({
-    id: "lx_jeonju",
-    url: "public/glb/siheung/siheung_demonstration.glb",
-    modelMatrix: siheung_model_matrix,
+const siheung_floor_model = Ditap.Model.fromGltf({
+    id: "siheung_floor_model",
+    url: "public/glb/siheung/siheung_demonstration_floor.glb",
+    modelMatrix: siheung_floor_model_matrix,
 });
+
+const siheung_wrong_model_matrix = new Ditap.Transforms.headingPitchRollToFixedFrame(
+    new Ditap.Cartesian3.fromDegrees(
+        127.058968,
+        35.835169,
+        34.97499999999889
+    ),
+    new Ditap.HeadingPitchRoll(Ditap.Math.toRadians(228), 0, 0),
+    Ditap.Ellipsoid.WGS84,
+    Ditap.Transforms.localFrameToFixedFrameGenerator("south", "east")
+);
+
+const siheung_wrong_model = Ditap.Model.fromGltf({
+    id: "siheung_wrong_model",
+    url: "public/glb/siheung/siheung_demonstration.glb",
+    modelMatrix: siheung_wrong_model_matrix,
+});
+
+
+
